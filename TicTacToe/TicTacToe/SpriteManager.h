95e0731dd51a22d35b645ca5b441d05023996a2e
Alexander <alleo92@hotmail.com>
2015-06-05 15:32:18 +0200

    adad h

---
+++ TicTacToe/TicTacToe/SpriteManager.h
@@ -0,0 +1,11 @@
+#include <string>
+
+
+const std::string SPRITE_X = "";
+const std::string SPRITE_O = "";
+const std::string SPRITE_NONE = "";
+
+
+const std::string SPRITE_BUTTON_HOST = "";
+const std::string SPRITE_BUTTON_START = "";
+const std::string SPRITE_BUTTON_EXIT = "";
